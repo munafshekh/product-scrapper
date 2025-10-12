42685e249add43b68f47ca03c1a9af37cf6f9fbd
Munaf <mnfshekh@gmail.com>
2025-10-12 19:32:28 +0100

    added some logic for the requests

--- main.py
+++ main.py
@@ -1,13 +1,62 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
-x = requests.get('https://w3schools.com/python/demopage.htm').text
-y = requests.get('https://google.com')
-amz = requests.get('https://www.amazon.com/CyberPowerPC-i9-14900KF-GeForce-Windows-GXiVR8080A39/dp/B0DW48QHFY/ref=sr_1_6?_encoding=UTF8&content-id=amzn1.sym.fe35c7fa-b4ae-4612-b878-056854c790ff&dib=eyJ2IjoiMSJ9.Z1gXDy0H8dNKp8LlcdArdu4Sy7HdabS9dhLdFtf7DEVkWaZRk8zyBPqLLXmnAACneoaG0LCylkRhgGXDG_YuOWXCuKX7tJWvue6OEos60Dreqznjc03RLQwT7xbBKLkixROKooEi3X5bXP4kGkLXmasPzKGjvjFtFbFkGp8VlmHzlG5fY4MYZRDeDLAbYzTS9vQXOdizjEkS3deq5-flBlUH3WHYRliel5DsNVfyKpK6MG84-PdLCTLxgg7RMuzMu4z6CvfLfWZLO1FCcnPROVr-8BGqHl5LkyxFol1UN0U.f3AFXZvLYOiMdn1bKlopzc1rE6fRRIJa_yXUgnCjM3w&dib_tag=se&keywords=gaming&pd_rd_r=1cd55f62-8ea6-4224-b495-b0b774430f56&pd_rd_w=xzfIq&pd_rd_wg=Xe2zM&qid=1760005995&sr=8-6')
+'''
+Need to install these libraries:
+bs4 for BeautifulSoup
+lxml (maybe not needed if you are using html parsing)
+requests
+'''
 
-# print(x)
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Connection': 'keep-alive',
+}
+
+
+# x = requests.get('https://w3schools.com/python/demopage.htm').text
+# y = requests.get('https://google.com')
+
+try:
+    url_amz = 'https://www.amazon.com/YILUSHENGHUA-Moisture-Proof-Container-Countertop-Kaleidoscope/dp/B0FPBF3GD6/ref=sr_1_1?_encoding=UTF8&sr=8-1'
+
+    amz_data = requests.get(url_amz, headers=headers, timeout=10)
+    amz_data.raise_for_status() #Checks if request was successful
+
+    print(f"Status Code : {amz_data.status_code}")
+
+    if amz_data.status_code == 200:
+        soup = BeautifulSoup(amz_data.content, "html.parser")
+
+
+        # Logic for parsing
+        title = soup.find("span", {"id": "productTitle"})
+        if title:
+            print(f"Product Title : {title.get_text().strip()}")
+        else:
+            print("Product title not found")
+        
+    else:
+        print(f"Recived status code : {amz_data.status_code}")
+        print("Amazon is blocking the request")
+
+
+except requests.exceptions.RequestException as e:
+    print(f"Request failed: {e}")
+
+
+
 
 # Parsing html content usinf beautifulsoup
-soup = BeautifulSoup(amz.text, "lxml")
-print(soup.prettify()) # Prints the parsed data fo html seems like you need to write this
-print("Testing git")
+# soup = BeautifulSoup(amz_data.text, "lxml")
+# print(soup.prettify()) # Prints the parsed data fo html seems like you need to write this
+# print("Testing git XOXO")
+
+# print(soup.find_all("productTitle"))
+
+
+# print(soup.title)
